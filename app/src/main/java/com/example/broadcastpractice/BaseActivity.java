fca526f6503668290c55b25c3ea4c21c884a429c
Xinghui Ding <hitapple271.com>
2020-07-30 14:42:43 +0800

    First commit.

---
+++ app/src/main/java/com/example/broadcastpractice/BaseActivity.java
@@ -0,0 +1,68 @@
+package com.example.broadcastpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ForceOffLineReceiver forceOffLineReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastpractice.FORCE_OFFLINE");
+        forceOffLineReceiver = new ForceOffLineReceiver();
+        registerReceiver(forceOffLineReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (forceOffLineReceiver != null){
+            unregisterReceiver(forceOffLineReceiver);
+            forceOffLineReceiver = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+    class ForceOffLineReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline. Please try login again later.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ActivityCollector.finishAll();
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);
+                }
+            });
+            builder.show();
+        }
+    }
+}
